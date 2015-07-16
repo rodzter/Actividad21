05740722170ee8b3e08e4ec3f15a4cc5dc865aa0
rodzter <rod_campos1@hotmail.com>
2015-07-16 13:19:33 -0500

    Initial commit

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ActiveRecord::Base
+  #la tabla==> article
+  #compos ==> article.title() ==> 'El titulo del articulo '
+  #escribir lo metodos
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Actividad21::Application.config.secret_key_base = '9a97a9cede412f2856b1b6c7f12e272e54a4894b8416d5eb47c8198cee2f0c542cf44c35e6bad52058eee42f130e87433860d5986c1b838f8e0754be358c74be'
